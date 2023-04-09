88a78080a0d73dfb9218e7c0bdddc638eb25d390
dannertech <danner@danner.tech>
2023-04-09 17:00:11 -0400

    added imports to Navbar and added BrowserRouter to App.jsx and components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { styles } from '../styles';
+import {navLinks} from '../constants';
+import {logo, menu, close} from '../assets';
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
