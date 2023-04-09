47cfc94b94ec98980d35c1b2c3ba0994a492eb80
dannertech <danner@danner.tech>
2023-04-09 19:10:42 -0400

    added code for menu and style changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,43 @@ import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
   return (
     <nav 
     className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link to="/" className='items-center gap-2'
+        <Link to="/" className='flex items-center gap-2'
         onClick={() => {
           setActive("")
           window.scrollTo(0,0);
         }
         }
         >
-        <img src={logo} alt={logo} className='w-0 h-9 object-contain'/>
+        <img src={logo} alt={logo} className='w-9 h-9 object-contain'/>
+        <p className='text-white text-[18px] font-bold cursor-pointer'>Danner<span className='sm:block hidden'>|Tech</span></p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => {
+            return (
+              <li key={link.id}
+              className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} 
+              onClick={() => {
+                setActive(link.title)
+              }}
+              >
+                <a href={`#${link.id}`}>
+                  {link.title}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+            <img className='w-[28px] h-[28px] object-contain cursor-pointer' alt="menu" src={menu}
+            onClick={() => setActive(!toggle)}
+            />
+        </div>
       </div>
     </nav>
   )
