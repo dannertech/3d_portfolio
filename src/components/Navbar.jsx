4e1b5c6f54d5a967ee132ceb51b8c5ea488c175a
dannertech <danner@danner.tech>
2023-04-09 17:23:58 -0400

    added logo to main page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,23 @@ import {logo, menu, close} from '../assets';
 import {Link} from 'react-router-dom';
 
 const Navbar = () => {
+  const [active, setActive] = useState('');
   return (
-    <div>Navbar</div>
+    <nav 
+    className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link to="/" className='items-center gap-2'
+        onClick={() => {
+          setActive("")
+          window.scrollTo(0,0);
+        }
+        }
+        >
+        <img src={logo} alt={logo} className='w-0 h-9 object-contain'/>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
